d05facb3fe84ba79e50b1cdec216ad42b9032fca
Andrew <andrewmbyrd@gmail.com>
2017-03-24 13:17:03 -0500

    Initialized project

---
+++ bloc_works_test.rb
@@ -0,0 +1,17 @@
+require 'bloc_works'
+require_relative './bloc_books'
+require 'rack/test'
+
+class BlocWorksTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    BlocWorks::Application
+  end
+
+  def test_it_puts_bloc_heads
+    get '/'
+    assert last_response.ok?
+  end
+
+end
