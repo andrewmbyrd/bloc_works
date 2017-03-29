6798318b2e5f9026258bbc3eaea44ae7c8faec7d
Andrew <andrewmbyrd@gmail.com>
2017-03-29 10:49:00 -0500

    working on router

--- bloc_works/lib/bloc_works/router.rb
+++ bloc_works/lib/bloc_works/router.rb
@@ -4,12 +4,13 @@ module BlocWorks
        #path info is in the URL. want the middle two things as those will correspond to
        #controller and action
        _, controller, action, _ = env["PATH_INFO"].split("/", 4)
+       ######binding.pry
        controller = controller.capitalize
        controller = "#{controller}Controller"
 
        #constant get turns a string into something like the Capitalized Class Name, so that we can actually have
        #the dynamic class name, and not just a string
-       
+
      end
 
      def fav_icon(env)
